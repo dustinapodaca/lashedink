0f56832824f78c8d0a4d6b4557910b84a487dd9d
Dustin Apodaca <dustinapodaca@gmail.com>
2023-04-25 20:07:07 -0600

    updates to services

--- src/Pages/Services/index.tsx
+++ src/Pages/Services/index.tsx
@@ -1,4 +1,5 @@
 import ServiceSelector from "../../Components/ServiceSelector";
+import ServiceCard from "../../Components/ServiceCard";
 
 // import brow1 from '../../assets/images/brows/brow1.jpg';
 import brow1 from '../../assets/images/brows/brow3.jpg';
@@ -32,14 +33,56 @@ const lashImages = [
 
 const Services = () => {
   return (
-    <div className="h-full bg-black px-4 pt-4 mt-20">
-      <div className="mx-auto flex h-full max-w-7xl flex-col justify-center rounded-xl">
-        <h1 className="text-white text-4xl md:text-4xl font-extrabold text-center mb-3 mt-3">
-          Services
-        </h1>
-        <div className="relative overflow-hidden rounded-xl">
-          <ServiceSelector images={browImages} text="Microblading" />
-          <ServiceSelector images={lashImages} text="Eyelashes" />
+    <div className="h-full bg-black mt-20">
+      <div className="mx-auto flex h-full max-w-7xl flex-col justify-center items-center rounded-xl">
+        <div
+          className="w-screen bg-zinc-800 shadow-lg overflow-hidden relative"
+          style={{ height: "550px" }}
+        >
+          <h1 className="text-white text-4xl md:text-4xl font-extrabold text-center mt-6 mb-2 font-poiret">
+            Services
+          </h1>
+          <div className="px-6 rounded-lg mb-8 text-sm text-white font-poiret">
+            <p className="my-4">
+              Please be advised that we have a 24-hour cancellation policy. If
+              you need to cancel or reschedule your appointment, please do so at
+              least 24 hours in advance. Failure to do so will result in a 50%
+              charge of the scheduled service.
+            </p>
+            <p className="mb-3">
+              Please note that we operate on an appointment-only basis.
+            </p>
+            <p className="mb-3">
+              Please arrive on time for your scheduled appointment. If you are
+              more than 10 minutes late, we may not be able to accommodate your
+              appointment and you may be subject to a cancellation fee.
+            </p>
+            <p className="mb-3">
+              Please inform us of any allergies or sensitivities you may have
+              prior to your appointment. We are not responsible for any allergic
+              reactions that may occur during or after your service.
+            </p>
+          </div>
+          <div className="absolute bottom-0 left-0 right-0 h-16">
+            <div
+              className="h-full w-full"
+              style={{
+                background: `linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 1) 100%)`,
+              }}
+            ></div>
+          </div>
+          <div className="flex justify-center">
+            <button className="mx-auto bg-black text-white py-2 px-6 font-poiret text-xl rounded-lg shadow-lg hover:bg-pink-600 focus:outline-none focus:ring-2 focus:ring-pink-600 focus:ring-offset-2">
+              Book Now
+            </button>
+          </div>
+        </div>
+
+        <div className="relative overflow-hidden rounded-xl mx-6">
+          <ServiceCard images={browImages} text="Microblading" />
+          <ServiceCard images={lashImages} text="Eyelashes" />
+          {/* <ServiceSelector images={browImages} text="Microblading" />
+          <ServiceSelector images={lashImages} text="Eyelashes" /> */}
         </div>
       </div>
     </div>
